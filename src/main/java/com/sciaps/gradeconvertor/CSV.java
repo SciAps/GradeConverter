49d99b88d8aa42b9b4d749682a19c0fca6853a16
paul <psoucy@sciaps.com>
2015-05-12 13:09:32 -0400

    added gradle build

--- src/main/java/com/sciaps/gradeconvertor/CSV.java
+++ src/main/java/com/sciaps/gradeconvertor/CSV.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gradeconvertor;
+package com.sciaps.gradeconvertor;
 
 import java.util.List;
 
